dd843f1ebb4e84140e1e93bff547028b0df08c93
刘知杭 <enterindex@gmail.com>
2019-03-18 01:01:57 +0800

    Updates

--- LParserC/Library.fs
+++ LParserC/Library.fs
@@ -6,17 +6,17 @@ module LParserC =
 
     [<Struct>]
     type StrStream = {
-        valu: string;
-        size: int;
-        line: int;
-        col: int;
+        valu:   string;
+        size:   int;
+        line:   int;
+        col:    int;
     }
 
     let inline createStrStream (s: string) = {
-            valu = s;
-            size = 0;
-            line = 1;
-            col = 1
+            valu    = s;
+            size    = 0;
+            line    = 1;
+            col     = 1
         }
 
     let inline getHead (self: StrStream) = self.valu.Chars self.size
@@ -40,6 +40,16 @@ module LParserC =
                     col = self.col + 1;
                 })
 
+    let inline highSlice s1 s2 =
+        assert (s1.size < s2.size)
+        let rec recSlice s1 s2 =
+            if s1.size = s2.size
+            then ""
+            else
+                let (f, l) = slice s1
+                string f + recSlice l s2
+        recSlice s1 s2
+
     // ParserC and ToolFunctions
 
     type Parserc = StrStream option -> StrStream option
@@ -47,25 +57,25 @@ module LParserC =
     // boxSay(foxSay), function = Packing
     let inline boxSay f v =
         match v with
-        | Some s -> f s
-        | None -> None
+        | Some s    -> f s
+        | None      -> None
 
     let inline (+) f1 f2 = f1 >> f2
 
-    let inline (*) (f1: Parserc) (f2: Parserc) = 
+    let inline (*) f1 f2 = 
         function
         | Some x ->
             match f1 (Some x) with
-            | Some t -> Some t
-            | None -> f2 (Some x)
+            | Some t    -> Some t
+            | None      -> f2 (Some x)
         | None -> None
 
     let rpt (f: Parserc) =
         let rec rf (v: StrStream option) =
             try
                 match f v with
-                | Some x -> rf (Some x)
-                | None -> v
+                | Some x    -> rf (Some x)
+                | None      -> v
             with
                 | :? OutOfRange -> v
         rf
@@ -97,7 +107,7 @@ module LParserC =
 
     let emptyParser = charParser ' ' * charParser '\n' * charParser '\t' * charParser '\r'
 
-    let inline parseSegm s =
+    let parseSegm =
         charParser '(' *
         charParser ')' *
         charParser '[' *
@@ -106,59 +116,41 @@ module LParserC =
         charParser '>' *
         charParser ',' *
         charParser '.' *
-        charParser ',' *
-        charParser '.' *
         emptyParser
 
 
     let numberParserR s =
         let (head, tail) = slice s
-        if head >= '0' && head <= '9' then Some tail else None
+        if head >= '0' && head <= '9'
+        then Some tail
+        else None
 
     let numberParser = boxSay numberParserR
 
-    let inline numbersParserR s = (rpt numberParser) s
-
-    let numbersParser = numbersParserR
+    let numbersParser = rpt numberParser
 
     let anyChar =
         boxSay ( fun s ->
             let (_, t) = slice s
             Some t)
 
-    let inline ConChar s =
-        s |> (charParser '\\' + anyChar) * anyChar
+    let ConChar = (charParser '\\' + anyChar) * anyChar
 
     // Export
 
-    let inline parseUint s =
-        s
-        |> numberParser
-        |> numbersParser
+    let parseBool = stringParser "false" * stringParser "true"
 
-    let inline parseInt s =
-        s
-        |> (charParser '+' * charParser '-')
-        |> parseUint
+    let parseUint = numberParser + numbersParser
 
-    let inline parseFloat s =
-        s |>
-        (parseInt * parseUint) + charParser '.' + parseUint
+    let parseInt = (charParser '+' * charParser '-') + parseUint
 
-    let inline parseRational s =
-        s |>
-        (parseInt * parseUint) |> charParser '/' |> (parseInt * parseUint)
+    let parseFloat = (parseInt * parseUint) + charParser '.' + parseUint
 
-    let inline parseChar s =
-        s 
-        |> charParser '\''
-        |> ConChar
-        |> charParser '\''
+    let parseRational = (parseInt * parseUint) + charParser '/' + (parseInt * parseUint)
 
-    let inline parseString s =
-        s 
-        |> charParser '\"'
-        |> rpt (charParser '\"' * ConChar)
+    let parseChar = charParser '\'' + ConChar + charParser '\''
+
+    let parseString = charParser '\"' + rpt (charParser '\"' * ConChar)
         
 // please use automatic curry
 // demo: (charParser 'a') s
